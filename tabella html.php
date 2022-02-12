b9d45812537b37e88b650cafef98c6a180f02d70
PaoloTarzia <92849640+PaoloTarzia@users.noreply.github.com>
2022-02-12 18:03:10 +0100

    Add files via upload

---
+++ tabella html.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Come creare una lista su html</title>
+</head>
+<body bgcolor="white">
+    <h1 style="color: blue;">come fare una tabella</h1>
+    <p>come fare una tabella</p>
+    <br>
+    <h2>cos'è una tabella?</h2>
+    <p>Le tabelle sono componenti importanti in HTML: nate agli inzi del Web per impaginare dati aggregati, si sono trasformate in uno strumento indispensabile per gestire i grafici layout.</p>
+    <p>come si crea una tabella?</p>
+    <xmp>
+        <table border="">
+            <tr><td>Colonna1</td><td>Colonna2</td></tr>
+            <tr><td>Dado 1,1</td><td>Dado 1,2</td></tr>
+            <tr><td>Dado 2,1</td><td>Dado 2,2</td></tr>
+        </table>
+    </xmp>
+
+    <p>esempio tabella</p>
+    <table border="";<table bgcolor="yellow" width="40%">
+        <tr><td>Colonna1</td><td>Colonna2</td></tr>
+        <tr><td>Dado 1,1</td><td>Dado 1,2</td></tr>
+        <tr><td>Dado 2,1</td><td>Dado 2,2</td></tr>
+    </table>
+</body>
+</html>
